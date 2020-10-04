31014beacf8136484e68096c6e6f526aebcb3e04
Rafael Guterres Jeffman <rafasgj@gmail.com>
2020-10-04 00:13:23 -0300

    Add simple application example.

---
+++ examples/greeting.py
@@ -0,0 +1,44 @@
+# This file is part of autocli
+#
+# Copyright (C) 2020 Rafael Guterres Jeffman
+#
+# f/π is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Foobar is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+"""autocli example: Greeting application."""
+
+import yaml
+
+from autocli.autocli import AutoCLI
+
+__CLI_CONFIGURATION = """
+---
+program: greeting
+description: A greeting application.
+version: 1.0
+handler: greeting.hello
+arguments:
+  - name: someone
+    description: Someone to greet.
+    required: true
+"""
+
+
+def hello(someone):
+    """Greet someone."""
+    print(f"Hello, {someone}!")
+
+
+if __name__ == "__main__":
+    cli = AutoCLI(yaml.safe_load(__CLI_CONFIGURATION))
+    cli.run()
